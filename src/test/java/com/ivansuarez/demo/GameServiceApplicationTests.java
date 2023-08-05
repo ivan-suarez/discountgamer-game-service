208b8b81f362b8310ee38df280f73022498c74e2
ivan-suarez <ivansuarezcast@gmail.com>
2023-08-05 22:58:49 +0200

    Initialize project

---
+++ src/test/java/com/ivansuarez/demo/GameServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ivansuarez.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GameServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

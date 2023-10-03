c96ee941520da4e09c880b8b16ed7b953af08d50
ivan-suarez <ivansuarezcast@gmail.com>
2023-10-03 19:10:58 +0200

    Rewrite basic authentication

--- src/main/java/com/ivansuarez/demo/config/SecurityConfig.java
+++ src/main/java/com/ivansuarez/demo/config/SecurityConfig.java
@@ -1,23 +1,44 @@
 package com.ivansuarez.demo.config;
 
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
-@EnableWebSecurity
-public class SecurityConfig{
-    
-    @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception{
-        http
-            .authorizeHttpRequests((authz) -> authz
-            .requestMatchers("/swagger-ui/**", "/swagger-resources/**", "/v2/api-docs", "/webjars/**").permitAll()
-            .anyRequest().authenticated()
-            )
-            .httpBasic();
+@Configuration
+public class SecurityConfig {
+
+   @Bean 
+    SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception{
+        http.authorizeHttpRequests(auth ->
+        auth.requestMatchers("/game/getAll").authenticated()
+        .anyRequest().permitAll())
+        .formLogin(Customizer.withDefaults())
+        .httpBasic(Customizer.withDefaults());
         return http.build();
     }
 
+    @Bean 
+    InMemoryUserDetailsManager inMemoryUserDetailsManager(){
+        UserDetails admin = User.withUsername("admin")
+        .password("admin")
+        .authorities("ADMIN")
+        .build();
+        UserDetails user = User.withUsername("user")
+        .password("user")
+        .authorities("ADMIN")
+        .build();
+        return new InMemoryUserDetailsManager(admin, user);
+    }
+
+    @Bean
+    PasswordEncoder passwordEncoder(){
+        return NoOpPasswordEncoder.getInstance();
+    }
 }
- 
